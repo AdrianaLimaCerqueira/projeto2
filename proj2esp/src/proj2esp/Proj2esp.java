cb99d619102cb6b16fdad96f3e592babdba14fc3
AdrianaLimaCerqueira <adrianalimacerqueira@gmail.com>
2017-09-14 01:27:16 +0100

    main
    erro na main

--- proj2esp/src/proj2esp/Proj2esp.java
+++ proj2esp/src/proj2esp/Proj2esp.java
@@ -17,13 +17,9 @@ public class Proj2esp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-/*<<<<<<< HEAD
         login log = new login();
-=======
-        // TODO code application logic here
         login login = new login();
         login.setVisible(true);
->>>>>>> 8c990236d2cdf947d810586a84e26c2390b51f61*/
     }
     
 }
